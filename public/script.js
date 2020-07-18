ac4aae62d4a795cf1e58613b8054371ba102d50c
Glitch (hello-express) <none>
2020-07-18 00:23:41 +0000

    👽🏟 Checkpoint ./public/script.js:1935498/111

--- public/script.js
+++ public/script.js
@@ -2,6 +2,9 @@ import htm from "https://cdn.pika.dev/htm@^3.0.4";
 import { createRouter } from "https://cdn.pika.dev/router5@^7.0.2";
 import browserRouter from "https://cdn.pika.dev/router5-plugin-browser@^7.0.2";
 
+import listenersPlugin from "router5/plugins/listeners";
+import browserPlugin from "router5/plugins/browser";
+
 const routes = [
   {name: 'home', path: '/'},
   {name: 'game', path: '/g/:gameName'},
