6179325e713ca22bcbd8e3b2b54948d8392e7cc7
Glitch (hello-express) <none>
2020-07-18 01:34:37 +0000

    🎐🌻 Checkpoint ./public/script.js:1935498/11358

--- public/script.js
+++ public/script.js
@@ -1,17 +1,15 @@
-import { h, render } from "https://cdn.pika.dev/preact@^10.4.4/";
-import { useState } from "https://cdn.pika.dev/preact@^10.4.4/hooks";
-import htm from "https://cdn.pika.dev/htm@^3.0.5";
-import { Link, Route, Switch } from "https://cdn.pika.dev/wouter-preact@^2.4.0";
-
-const html = htm.bind(h);
+import { h,  html, render, useState, Link, Route, Switch } from "https://unpkg.com/swree?module";
 
 function App() {
+  console.log(Link, Route, Switch);
   return html`
     <div>
       <h1>Hello, Preact!</h1>
       <p>From htm, with love</p>
 
-      <${Route} path="/"><${GamesList} /><//>
+      <!--- <${Switch}> --->
+        <${Route} path="/"><${GamesList} /><//>
+      <!--- <//> --->
     </div>
   `;
 }
@@ -26,4 +24,4 @@ function GamesList() {
   `;
 }
 
-render(html`<${App}/>`, document.querySelector("#target"));
+render(App(), document.querySelector("#target"));
