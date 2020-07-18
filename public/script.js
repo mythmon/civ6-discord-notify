967c8cd2d5bf5521ebe453c8efb015542448676c
Glitch (hello-express) <none>
2020-07-18 00:16:57 +0000

    🌽🗃 Checkpoint ./public/script.js:1935498/410 ./public/index.html:1935498/25

--- public/script.js
+++ public/script.js
@@ -1,9 +1,21 @@
-import * as htm from "https://cdn.pika.dev/htm@^3.0.4";
-import * as createRouter from "https://cdn.pika.dev/router5@^7.0.2";
-import * as browserRouter from "https://cdn.pika.dev/router5-plugin-browser@^7.0.2";
+import htm from "https://cdn.pika.dev/htm@^3.0.4";
+import { createRouter } from "https://cdn.pika.dev/router5@^7.0.2";
+import browserRouter from "https://cdn.pika.dev/router5-plugin-browser@^7.0.2";
+
+const routes = [
+  {name: 'home', path: '/'},
+  {name: 'game', path: '/g/:gameName'},
+];
 
 const router = createRouter();
 
 router.usePlugin(browserRouter());
 
+router.subscribe(({route, previousRoute}) => {
+  console.log("navigated to", {route}, "from", {previousRoute});
+})
+
 router.start();
+console.log(router.getState());
+router.navigate("/");
+console.log(router.getState());
