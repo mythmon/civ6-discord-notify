9d7aaaf97c0aa5337b004a7d7e261e5c24931ee2
Michael Cooper <mythmon@gmail.com>
2020-07-21 19:20:05 -0700

    Add cors

--- server/main.js
+++ server/main.js
@@ -1,5 +1,6 @@
 const path = require("path");
 
+const cors = require("cors");
 const express = require("express");
 const fallback = require("express-history-api-fallback");
 
@@ -11,6 +12,7 @@ const root = path.resolve(__dirname + "/../public");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static(root));
 
